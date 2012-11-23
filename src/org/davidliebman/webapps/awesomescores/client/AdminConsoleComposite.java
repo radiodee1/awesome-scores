829befafb5882f2fb33a52e13a08511c9b6811c2
david.c.liebman <david.c.liebman@9818806b-2cdb-3562-6598-f161770dbe05>
2012-11-23 18:15:27 +0000

    adding parts of adminConsole - detect targetGame string

--- src/org/davidliebman/webapps/awesomescores/client/AdminConsoleComposite.java
+++ src/org/davidliebman/webapps/awesomescores/client/AdminConsoleComposite.java
@@ -1,15 +1,21 @@
 package org.davidliebman.webapps.awesomescores.client;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Button;
 
-public class AdminConsoleComposite extends Composite {
+public class AdminConsoleComposite extends Composite implements HasClickHandlers{
 
 	private TextBox gameEmail, gameName, gameScore, gameLevel, gameLives, gameSpeed;
-	//private Button gameButton;
+	private Button gameButton;
+	private boolean buttonPressed = false;
+	
 	
 	public AdminConsoleComposite() {
 		
@@ -20,47 +26,57 @@ public class AdminConsoleComposite extends Composite {
 		Label lblNameemailAddress = new Label("Name/Email Address:");
 		verticalPanel.add(lblNameemailAddress);
 		
-		TextBox txtbxUsergmailcom = new TextBox();
-		txtbxUsergmailcom.setText("user@gmail.com");
-		verticalPanel.add(txtbxUsergmailcom);
+		gameEmail = new TextBox();
+		gameEmail.setText("user@gmail.com");
+		verticalPanel.add(gameEmail);
 		
 		Label lblGameName = new Label("Game Name:");
 		verticalPanel.add(lblGameName);
 		
-		TextBox txtbxUser = new TextBox();
-		txtbxUser.setText("user");
-		verticalPanel.add(txtbxUser);
+		gameName = new TextBox();
+		gameName.setText("user");
+		verticalPanel.add(gameName);
 		
 		Label lblHighScore = new Label("High Score:");
 		verticalPanel.add(lblHighScore);
 		
-		TextBox textBox = new TextBox();
-		textBox.setText("1000");
-		verticalPanel.add(textBox);
+		gameScore = new TextBox();
+		gameScore.setText("1000");
+		verticalPanel.add(gameScore);
 		
 		Label lblLevel = new Label("Level:");
 		verticalPanel.add(lblLevel);
 		
-		TextBox textBox_1 = new TextBox();
-		textBox_1.setText("1");
-		verticalPanel.add(textBox_1);
+		gameLevel = new TextBox();
+		gameLevel.setText("1");
+		verticalPanel.add(gameLevel);
 		
 		Label lblLives = new Label("Lives:");
 		verticalPanel.add(lblLives);
 		
-		TextBox textBox_2 = new TextBox();
-		textBox_2.setText("3");
-		verticalPanel.add(textBox_2);
+		gameLives = new TextBox();
+		gameLives.setText("3");
+		verticalPanel.add(gameLives);
 		
 		Label lblSpeed = new Label("Speed:");
 		verticalPanel.add(lblSpeed);
 		
-		TextBox textBox_3 = new TextBox();
-		textBox_3.setText("40");
-		verticalPanel.add(textBox_3);
+		gameSpeed = new TextBox();
+		gameSpeed.setText("40");
+		verticalPanel.add(gameSpeed);
 		
-		Button btnSubmitScores = new Button("Submit Scores");
-		verticalPanel.add(btnSubmitScores);
+		gameButton = new Button("Submit Scores");
+		verticalPanel.add(gameButton);
+		
+		gameButton.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				buttonPressed = true;
+				
+			}
+			
+		});
 	}
 
 	public String getGameEmail() {
@@ -110,5 +126,31 @@ public class AdminConsoleComposite extends Composite {
 	public void setGameSpeed(String gameSpeed) {
 		this.gameSpeed.setText(gameSpeed);
 	}
+	
+	
+
+	public Button getGameButton() {
+		return gameButton;
+	}
+
+//	public void setGameButton(Button gameButton) {
+//		this.gameButton = gameButton;
+//	}
+
+	@Override
+	public HandlerRegistration addClickHandler(ClickHandler handler) {
+		// TODO Auto-generated method stub
+		return addDomHandler(handler, ClickEvent.getType());
+	}
+
+	public boolean isButtonPressed() {
+		return buttonPressed;
+	}
+
+	public void setButtonPressed(boolean buttonPressed) {
+		this.buttonPressed = buttonPressed;
+	}
+	
+	
 	
 }
