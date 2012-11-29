6f3769d4b1b63b3226fbf43ba00e3e13b7de3c03
david.c.liebman <david.c.liebman@9818806b-2cdb-3562-6598-f161770dbe05>
2012-11-29 12:36:16 +0000

    add new entrypoint

---
+++ src/org/davidliebman/webapps/awesomescores/client/Awesome_scores_admin.java
@@ -0,0 +1,12 @@
+package org.davidliebman.webapps.awesomescores.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class Awesome_scores_admin implements EntryPoint {
+
+	public void onModuleLoad() {
+		RootPanel rootPanel = RootPanel.get();
+	}
+
+}
