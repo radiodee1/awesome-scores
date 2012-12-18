de2fa14cde15b2b4e44d10bfaecdd288fde04109
david.c.liebman@gmail.com <david.c.liebman@gmail.com@9818806b-2cdb-3562-6598-f161770dbe05>
2012-12-18 17:21:46 +0000

    adding android-awesomescores to project

---
+++ testapp/android-awesomescores/gen/org/davidliebman/android/androidawesomescores/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.davidliebman.android.androidawesomescores;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ testapp/android-awesomescores/src/org/davidliebman/android/androidawesomescores/MainActivity.java
@@ -0,0 +1,158 @@
+package org.davidliebman.android.androidawesomescores;
+
+import java.io.ByteArrayOutputStream;
+import java.io.Console;
+import java.io.IOException;
+//import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.webscale.json.client.JsonClient;
+import org.webscale.json.cmd.AppEngineCmdResults;
+import org.webscale.json.cmd.JsonCmd;
+
+import com.google.gson.Gson;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	
+
+		Button mGoButton = (Button) findViewById(R.id.button1);
+	    mGoButton.setOnClickListener(new OnClickListener() {
+	    	@Override
+	        public void onClick(View v) {
+				TextView mtext = (TextView) findViewById(R.id.text_output);
+	        	
+	    		new AsyncTask <Object, Object, Object>() {
+
+	    			 @Override
+	    		        protected void onPreExecute() {
+	    		            // TODO Auto-generated method stub
+	    		            //super.onPostExecute(result);
+	    		            Toast.makeText(MainActivity.this, "task started.", Toast.LENGTH_LONG).show();
+	    		        }
+	    			
+					@Override
+					protected Object doInBackground(Object... params) {
+						TextView mtext = (TextView) findViewById(R.id.text_output);
+
+			        	//mtext.setText(someExecuteMethod());
+						return someExecuteMethod();
+					}
+					
+					 @Override
+				        protected void onPostExecute(Object result) {
+				            // TODO Auto-generated method stub
+				            //super.onPostExecute(result);
+				            Toast.makeText(MainActivity.this, (String) result, Toast.LENGTH_LONG).show();
+				        }
+	    		}.execute(new Object());
+	    		//mtext.setText("waiting...2");
+	        	
+	        	Toast.makeText(MainActivity.this, "And We're Off", Toast.LENGTH_SHORT).show();
+	        }
+	
+			
+	    });
+    
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	public String someExecuteMethod()  {
+		Record rec = new Record();
+		
+		rec.setName("hello world");
+		rec.setAndroidAppname("org.davidliebman.android.awesomeguy");
+		rec.setEmail("hello-user@gmail.com");
+		
+		Gson gson = new Gson();
+		String cmd = gson.toJson(rec);
+		Log.e("org.davidliebman", "command="+cmd);
+//		TextView mtext = (TextView) findViewById(R.id.text_output);
+//		mtext.setText(cmd);
+        //String cmd = new String(rec.getName());
+        String res = null;
+        try {
+                res = sendToJsonClient(cmd);
+                //Console.pt(res.getResponse());
+        } catch (Exception e) {
+                e.printStackTrace();
+                //throw new Exception("fail" ,e);
+        }
+        return res;
+        
+	}
+
+	public String sendToJsonClient(String cmd) throws Exception{
+		String responseString = new String();
+		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+		nameValuePairs.add(new BasicNameValuePair("command", cmd));
+		
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost("http://10.0.2.2:8888/game/game.html");
+		httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+		
+		cmd = cmd.substring(1, cmd.length()- 1);
+	    HttpResponse response = httpclient.execute(httppost);//?command="+cmd));
+	    StatusLine statusLine = response.getStatusLine();
+	    if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+	        ByteArrayOutputStream out = new ByteArrayOutputStream();
+	        response.getEntity().writeTo(out);
+	        out.close();
+	        responseString = out.toString();
+	        //..more logic
+	    } else{
+	        //Closes the connection.
+	        response.getEntity().getContent().close();
+	        throw new IOException(statusLine.getReasonPhrase());
+	    }		
+	    
+//        JsonClient<String> client = new JsonClient<String>();
+//        client.setUuid(UUID.randomUUID().toString());
+//        //client.setSubmissionURL("http://yourapp.appspot.com/JSONServlet");
+//        client.setSubmissionURL("http://localhost:8888/game/game.html");
+//        client.setObjTosubmit(cmd);
+//        client.setJsonRequestId("jsonCommandHandler");
+//        client.setResponseClazz(AppEngineCmdResults.class);
+//        
+//        String response = client.call();
+//        return response;
+	    
+	    return responseString;
+	}
+
+}
