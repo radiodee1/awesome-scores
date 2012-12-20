133173fde005b2031e2e581be99b21967ac8bc33
david.c.liebman <david.c.liebman@9818806b-2cdb-3562-6598-f161770dbe05>
2012-12-20 22:29:41 +0000

    scores stuff

--- src/org/davidliebman/webapps/awesomescores/server/JSONDataCollector.java
+++ src/org/davidliebman/webapps/awesomescores/server/JSONDataCollector.java
@@ -1,34 +1,104 @@
 package org.davidliebman.webapps.awesomescores.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 import org.davidliebman.webapps.awesomescores.shared.Record;
-import org.webscale.json.JsonRequestHandler;
-//import org.webscale.json.cmd.AppEngineCmdResults;
-//import org.webscale.json.cmd.JsonCmd;
 
 import com.google.gson.Gson;
 
-public class JSONDataCollector implements JsonRequestHandler{
+public class JSONDataCollector {
 
+	
+	public static final String ERROR = new String("ERROR");
 	
 	ScoreManagerJDO manageJDO;// = new ScoreManagerJDO();
 
+	private String jsonIn = new String();
+	private String jsonOut = new String();
 	 //private static final Logger log = Logger.getLogger(JsonCmdHandler.class);
+	private Record rec = new Record();
      
-     @Override
-     public String executeRequest(String jsonRequestString) {
-             //log.debug("JSON String received-"+ jsonRequestString);
+	public JSONDataCollector () {
+		
+	}
+	
+	
+	public void executeRequest() {
+		setJsonOut(executeRequest(getJsonIn()));
+	}
+	
+     private String executeRequest(String jsonRequestString) {
              Gson gson = new Gson();
-             Record rec = gson.fromJson(jsonRequestString, Record.class);
+             try {
+             
+             rec = gson.fromJson(jsonRequestString, Record.class);
+             
+             }
+             catch(Exception e) {
+            	 return new String(ERROR+"1");
+             }
+             
              Long keyReturned = new Long(0);
              //do some thing with the cmd on server
              manageJDO = new ScoreManagerJDO();
              keyReturned = manageJDO.saveRecord(new RecordSaver(rec,null));
-             //AppEngineCmdResults results = ((Object) cmd).execute(cmd.getParameters());
-             gson = new Gson();
-             String resultJson = new String(keyReturned.toString());
-             //String resultJson = gson.toJson(rec);
+
+             String resultJson = new String();
+             if (keyReturned == 0 ) {
+            	 resultJson = new String(ERROR+"2");
+             }
+             
+             
+             resultJson = new String(keyReturned.toString());
+             
              return resultJson;
      }
 
+
+     public void testMe() {
+    	 this.setJsonOut(jsonIn);
+     }
+     
+	public String getJsonIn() {
+		return jsonIn;
+	}
+
+
+	public void setStringJsonIn(String jsonIn) {
+		this.jsonIn = jsonIn;
+	}
+
+	public void setJsonIn(BufferedReader in) {
+		try {
+			String sCurrentLine;
+			 
+			while ((sCurrentLine = in.readLine()) != null) {
+				setStringJsonIn(this.jsonIn + sCurrentLine);
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String getJsonOut() {
+		return jsonOut;
+	}
+
+
+	public void setJsonOut(String jsonOut) {
+		this.jsonOut = jsonOut;
+	}
+
+	public Record getRec() {
+		return rec;
+	}
+
+	public void setRec(Record rec) {
+		this.rec = rec;
+	}
+
+    
 	
 }
--- war/game.jsp
+++ war/game.jsp
@@ -1,1 +1,39 @@
-test <% out.println("hello"); %>
+test 
+<%@ page import = " java.util.* " %>
+
+<% 
+
+//import java.util;
+
+//out.println("hello"); 
+org.davidliebman.webapps.awesomescores.server.JSONDataCollector scores = new
+org.davidliebman.webapps.awesomescores.server.JSONDataCollector();
+scores.setJsonIn(request.getReader());
+scores.executeRequest();
+out.println(scores.getJsonOut());
+
+out.println("<br>\n");
+
+Enumeration enumeration = request.getHeaderNames();
+
+while (enumeration.hasMoreElements()) {
+
+String string = (String)enumeration.nextElement();
+
+out.println("<font size = 3>" +string +": " + request.getHeader(string)+ "</font><br>");
+
+}
+out.println("----------------------<br>\n");
+
+out.println(request.getMethod() + "xx");
+
+Enumeration attributes = request.getParameterNames();
+
+while (attributes.hasMoreElements()) {
+
+String string = (String)attributes.nextElement();
+
+out.println("<font size = 3>" +string +": " + request.getParameter(string)+ "</font><br>");
+
+}
+%>
